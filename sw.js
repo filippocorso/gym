4f822dab47b7aec1b759f95f9a9e042829a68d2d
filippocorso <147344876+filippocorso@users.noreply.github.com>
2025-11-07 17:18:47 +0100

    Remove console logs from service worker events
    Removed console log statements from install and activate events.

--- sw.js
+++ sw.js
@@ -1,8 +1,5 @@
 self.addEventListener("install", () => {
-  console.log("Service Worker installato");
   self.skipWaiting();
 });
 
-self.addEventListener("activate", () => {
-  console.log("Service Worker attivo");
-});
+self.addEventListener("fetch", () => {});
