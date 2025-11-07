fc810b2d995b3bca529d3b9e6bae11adc3907ccd
filippocorso <147344876+filippocorso@users.noreply.github.com>
2025-11-07 17:30:50 +0100

    Add activate event and enhance fetch handling

--- sw.js
+++ sw.js
@@ -2,4 +2,13 @@ self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("fetch", () => {});
+self.addEventListener("activate", () => {
+  caches.keys().then(keys =>
+    keys.forEach(key => caches.delete(key))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(fetch(event.request));
+});
