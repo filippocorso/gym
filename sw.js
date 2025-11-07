8c0fb62d097fcfb3fd016138153d0b7104107333
filippocorso <147344876+filippocorso@users.noreply.github.com>
2025-11-07 22:06:45 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,12 @@
-self.addEventListener('install',event=>{console.log('SW install');});
-self.addEventListener('activate',event=>{console.log('SW activate');});
-self.addEventListener('fetch',event=>{});
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+
+self.addEventListener('message', event => {
+  if(event.data && event.data.type === 'playBeep'){
+    // Show notification (will vibrate / show depending on platform)
+    self.registration.showNotification('Recupero terminato!', {
+      body: 'Inizia la prossima serie',
+      silent: false
+    });
+  }
+});
