61c5cc7293d9d94df66e15d4d3429d9d5432abca
filippocorso <147344876+filippocorso@users.noreply.github.com>
2025-11-07 17:35:20 +0100

    Remove service worker event listeners

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-self.addEventListener("install", () => {
-  self.skipWaiting();
-});
-
-self.addEventListener("activate", () => {
-  caches.keys().then(keys =>
-    keys.forEach(key => caches.delete(key))
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
-});
+//prova
