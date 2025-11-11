998a4dfcd4de97bdcbc9e6e2ec5b5ed119b44747
filippocorso <147344876+filippocorso@users.noreply.github.com>
2025-11-11 23:14:25 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,12 @@
+self.addEventListener('install', e => { self.skipWaiting(); });
+self.addEventListener('activate', e => { self.clients.claim(); });
+
+self.addEventListener('message', event => {
+  if(event.data && event.data.type === 'playBeep'){
+    self.registration.showNotification('Recupero terminato!', {
+      body: 'Inizia la prossima serie',
+      silent: false
+    });
+  }
+});
 
