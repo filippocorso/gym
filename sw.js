b3c9b58dde5245e7e1d4931f27c22a954873d117
filippocorso <147344876+filippocorso@users.noreply.github.com>
2025-11-11 21:39:50 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,6 @@ self.addEventListener('activate', e => { self.clients.claim(); });
 
 self.addEventListener('message', event => {
   if(event.data && event.data.type === 'playBeep'){
-    // show a notification so user is alerted (sound/visual depending on platform)
     self.registration.showNotification('Recupero terminato!', {
       body: 'Inizia la prossima serie',
       silent: false
